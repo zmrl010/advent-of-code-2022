0c2a8414ac44bf06ccdfcfbef7a9c38a936c3b0f
Zach <zmriel@gmail.com>
2022-12-01 22:42:47 -0500

    day 1, part 2

--- calendar/1_calorie_counting/src/lib.rs
+++ calendar/1_calorie_counting/src/lib.rs
@@ -4,8 +4,19 @@
 
 type Integer = u32;
 
-pub fn find_largest_calorie_cluster(input: &str) -> Option<Integer> {
+/// Calculate the sum of each calorie cluster and find the largest one
+pub fn find_max_calorie_cluster(input: &str) -> Option<Integer> {
     input.split("\n\n").map(calculate_cluster_sum).max()
+}
+
+/// Calculate the sum of each calorie cluster and return the sum of the top n from those
+pub fn sum_n_max_calorie_clusters(input: &str, n: usize) -> Integer {
+    let mut sums = input
+        .split("\n\n")
+        .map(calculate_cluster_sum)
+        .collect::<Vec<Integer>>();
+    sums.sort_unstable_by(|a, b| b.cmp(a));
+    sums.iter().take(n).sum()
 }
 
 /// Take a line separated list of integers and calculate the sum of the entire cluster
@@ -31,20 +42,27 @@ mod tests {
 
     use anyhow::{anyhow, Context};
 
-    use crate::find_largest_calorie_cluster;
+    use super::*;
 
     const BASIC_EXAMPLE: &str =
         "1000 \n2000 \n3000 \n\n4000 \n\n5000 \n6000 \n\n7000 \n8000 \n9000 \n\n10000 \n";
 
     #[test]
-    fn should_find_largest_cluster_is_24000_in_basic_example() {
-        let result = find_largest_calorie_cluster(BASIC_EXAMPLE);
+    fn find_max_calorie_cluster_should_be_24000_in_basic_example() {
+        let result = find_max_calorie_cluster(BASIC_EXAMPLE);
 
         assert_eq!(result, Some(24000));
     }
 
     #[test]
-    fn should_compute_input() -> anyhow::Result<()> {
+    fn find_3_max_calorie_clusters_sum_should_be_45000_in_basic_example() {
+        let result = sum_n_max_calorie_clusters(BASIC_EXAMPLE, 3);
+
+        assert_eq!(result, 45000);
+    }
+
+    #[test]
+    fn should_find_max_calorie_cluster_input() -> anyhow::Result<()> {
         let input_path = {
             let mut dir = PathBuf::from(env!("CARGO_MANIFEST_DIR"));
             dir.push("input");
@@ -53,9 +71,26 @@ mod tests {
 
         let input = fs::read_to_string(&input_path)
             .with_context(|| anyhow!("failed to read file at `{}`", input_path.display()))?;
-        let result = find_largest_calorie_cluster(input.as_str());
+        let result = find_max_calorie_cluster(input.as_str());
 
         assert_eq!(result, Some(72602));
+
+        Ok(())
+    }
+
+    #[test]
+    fn should_find_3_max_calorie_clusters_sum_using_input() -> anyhow::Result<()> {
+        let input_path = {
+            let mut dir = PathBuf::from(env!("CARGO_MANIFEST_DIR"));
+            dir.push("input");
+            dir
+        };
+
+        let input = fs::read_to_string(&input_path)
+            .with_context(|| anyhow!("failed to read file at `{}`", input_path.display()))?;
+        let result = sum_n_max_calorie_clusters(input.as_str(), 3);
+
+        assert_eq!(result, 207410);
 
         Ok(())
     }
