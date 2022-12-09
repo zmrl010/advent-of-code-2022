f7c1df35484c17bd2139e67002b33719a6be71bd
Zach <zmriel@gmail.com>
2022-12-09 17:26:35 -0500

    day 5, part 2
    (FINALLY!!!)

--- calendar/5_supply_stack/src/collections.rs
+++ calendar/5_supply_stack/src/collections.rs
@@ -1,60 +1,122 @@
-pub mod stack {
-    use crate::Crate;
-    use std::vec::IntoIter;
+pub mod cratemap {
+    use std::{
+        collections::{
+            hash_map::{Entry, IntoIter},
+            HashMap,
+        },
+        str::FromStr,
+    };
 
-    /// Simple Stack (LIFO) data structure
+    use crate::{error::ParseError, Crate};
+
+    /// Map string keys to crate collections
     #[derive(Debug, PartialEq, Eq, Default)]
-    pub struct Stack(Vec<Crate>);
+    pub struct CrateMap<V> {
+        map: HashMap<String, V>,
+    }
 
-    impl Stack {
+    impl<V: Default> CrateMap<V> {
+        /// Initialize an empty map
         pub fn new() -> Self {
-            Self(Vec::new())
-        }
-
-        pub fn pop(&mut self) -> Option<Crate> {
-            self.0.pop()
-        }
-
-        pub fn push(&mut self, val: Crate) {
-            self.0.push(val)
+            Default::default()
         }
     }
 
-    impl<const N: usize> From<[char; N]> for Stack {
-        fn from(s: [char; N]) -> Self {
-            Self(Vec::from(s))
+    impl<V> CrateMap<V> {
+        /// Exposes the entry api from the underlying HashMap
+        pub fn entry(&mut self, key: String) -> Entry<'_, String, V> {
+            self.map.entry(key)
         }
     }
 
-    impl IntoIterator for Stack {
-        type Item = Crate;
-        type IntoIter = IntoIter<Self::Item>;
+    impl<V> IntoIterator for CrateMap<V> {
+        type Item = (String, V);
+        type IntoIter = IntoIter<String, V>;
 
         fn into_iter(self) -> Self::IntoIter {
-            self.0.into_iter()
+            self.map.into_iter()
         }
     }
-}
 
-pub mod queue {
-    use crate::Crate;
-    use std::collections::VecDeque;
+    impl<V: Default + Extend<Crate>> FromIterator<(String, Crate)> for CrateMap<V> {
+        fn from_iter<T: IntoIterator<Item = (String, Crate)>>(iter: T) -> Self {
+            let mut map = Self::new();
 
-    /// Simple Queue (FIFO) data structure
-    #[derive(Debug, PartialEq, Eq, Default)]
-    pub struct Queue(VecDeque<Crate>);
+            for (key, crate_) in iter {
+                let stack = map.entry(key).or_default();
+                stack.extend([crate_])
+            }
 
-    impl Queue {
-        pub fn new() -> Self {
-            Self(VecDeque::new())
+            map
         }
+    }
 
-        pub fn pop_front(&mut self) -> Option<Crate> {
-            self.0.pop_front()
+    impl<V: Default + Extend<Crate>> FromStr for CrateMap<V> {
+        type Err = ParseError;
+
+        fn from_str(s: &str) -> Result<Self, Self::Err> {
+            let mut rows = s.lines().rev();
+            let index_row = rows.next().ok_or_else(|| ParseError::ParseStringError {
+                from: s.to_string(),
+            })?;
+
+            let mut map = CrateMap::new();
+
+            let key_indices: Vec<(usize, String)> = index_row
+                .match_indices(char::is_numeric)
+                .map(|(i, key)| (i, key.to_string()))
+                .collect();
+
+            for (column_index, key) in key_indices {
+                let crates: &mut V = map.entry(key).or_default();
+
+                crates.extend(rows.clone().filter_map(|row| {
+                    let char = match row.chars().nth(column_index) {
+                        Some(' ') | None => None,
+                        Some(char) => Some(char),
+                    }?;
+                    Some(char)
+                }));
+            }
+
+            // let map_iterator = rows.flat_map(|row| {
+            //     key_indices.iter().filter_map(|(col_index, key)| {
+            //         let col = match row.chars().nth(*col_index) {
+            //             Some(' ') | None => None,
+            //             Some(col) => Some(col),
+            //         }?;
+            //         Some((key.to_string(), col))
+            //     })
+            // });
+
+            Ok(map)
         }
+    }
 
-        pub fn push_back(&mut self, val: Crate) {
-            self.0.push_back(val)
+    #[cfg(test)]
+    mod tests {
+        use crate::{Crate, CrateMap, ParseError};
+
+        const BASIC_EXAMPLE: &str = "    [D]    \n[N] [C]    \n[Z] [M] [P]\n 1   2   3 ";
+
+        #[test]
+        fn basic_example_should_parse_into_cratemap() -> Result<(), ParseError> {
+            let result: CrateMap<Vec<Crate>> = BASIC_EXAMPLE.parse()?;
+
+            let expected = {
+                let mut map = CrateMap::new();
+
+                map.entry("1".to_string()).or_insert(Vec::from(['Z', 'N']));
+                map.entry("2".to_string())
+                    .or_insert(Vec::from(['M', 'C', 'D']));
+                map.entry("3".to_string()).or_insert(Vec::from(['P']));
+
+                map
+            };
+
+            assert_eq!(result, expected);
+
+            Ok(())
         }
     }
 }
--- calendar/5_supply_stack/src/lib.rs
+++ calendar/5_supply_stack/src/lib.rs
@@ -1,10 +1,10 @@
 mod collections;
-mod cratemap;
 mod error;
 mod procedure;
 
-pub use collections::{queue::Queue, stack::Stack};
-use cratemap::CrateMap;
+use std::iter::Extend;
+
+pub use collections::cratemap::CrateMap;
 use error::{Error, ParseError};
 pub use procedure::{Instruction, Procedure};
 
@@ -12,45 +12,96 @@ pub type Crate = char;
 
 pub type Result<T, E = Error> = std::result::Result<T, E>;
 
-pub fn collect_message(map: CrateMap) -> String {
-    let mut chars: Vec<(String, char)> = Vec::new();
+pub fn collect_message(map: CrateMap<Vec<Crate>>) -> String {
+    let mut chars: Vec<(String, char)> = map
+        .into_iter()
+        .filter_map(|(key, crates)| {
+            let cr = crates.last()?;
+            Some((key, *cr))
+        })
+        .collect();
 
-    for (key, mut stack) in map {
-        if let Some(cr) = stack.pop() {
-            chars.push((key, cr))
-        }
-    }
-
+    // keys are only still here so we can sort at the end (rethink this)
     chars.sort_by(|(key_a, _), (key_b, _)| key_a.cmp(key_b));
-
+    // iterate again to remove keys
     chars.iter().map(|(_, value)| value).collect()
 }
 
-pub fn rearrange_crates(input: &str) -> Result<CrateMap> {
+fn move_crates_one_by_one(instruction: Instruction, map: &mut CrateMap<Vec<Crate>>) {
+    let Instruction {
+        num_crates,
+        source,
+        target,
+    } = instruction;
+
+    let source_values = {
+        let source: &mut Vec<Crate> = map.entry(source.clone()).or_default();
+
+        /*
+            we need an intermediary vec so we can store the values and drop the mutable
+            reference to source in the HashMap before we can get another mutable reference
+            to the target in the HashMap. This avoids needing any fancy smart pointers
+        */
+        let mut values: Vec<Crate> = Vec::with_capacity(num_crates as usize);
+
+        let mut count = num_crates;
+        while count > 0 {
+            if let Some(value) = source.pop() {
+                values.push(value);
+            }
+
+            count -= 1;
+        }
+
+        values
+    };
+
+    map.entry(target.clone()).or_default().extend(source_values);
+}
+
+fn move_crates_all(instruction: Instruction, map: &mut CrateMap<Vec<Crate>>) {
+    let Instruction {
+        num_crates,
+        source,
+        target,
+    } = instruction;
+    let num_crates = num_crates as usize;
+
+    let source_values = {
+        let source: &mut Vec<Crate> = map.entry(source.clone()).or_default();
+
+        let at = source.len() - num_crates.min(source.len());
+
+        let values = source.split_off(at);
+
+        values
+    };
+
+    map.entry(target.clone()).or_default().extend(source_values);
+}
+
+pub fn rearrange_crates(input: &str) -> Result<CrateMap<Vec<Crate>>> {
     let (procedure, mut map) = match parse_input(input) {
         Ok(val) => val,
         Err(e) => return Err(Error::from(e)),
     };
 
-    for Instruction {
-        num_crates,
-        source,
-        target,
-    } in procedure
-    {
-        let values = {
-            // TODO should we .or_default() here?
-            let source_stack = map.entry(source.clone()).or_default();
+    for instruction in procedure {
+        move_crates_one_by_one(instruction, &mut map);
+    }
 
-            let values: Vec<Crate> = (0..num_crates).filter_map(|_| source_stack.pop()).collect();
-            values
-        };
+    Ok(map)
+}
 
-        let target_stack = map.entry(target.clone()).or_default();
+/// Part 2 - using a queue data structure to retain order
+pub fn rearrange_crates_part2(input: &str) -> Result<CrateMap<Vec<Crate>>> {
+    let (procedure, mut map) = match parse_input(input) {
+        Ok(val) => val,
+        Err(e) => return Err(Error::from(e)),
+    };
 
-        for value in values {
-            target_stack.push(value)
-        }
+    for instruction in procedure {
+        move_crates_all(instruction, &mut map)
     }
 
     Ok(map)
@@ -61,12 +112,12 @@ pub fn rearrange_crates(input: &str) -> Result<CrateMap> {
 /// * Top half is a map of the crate stacks - [`CrateMap`]
 /// * Bottom half is instructions to rearrange the stacks - [`Procedure`]
 ///
-pub fn parse_input(input: &str) -> Result<(Procedure, CrateMap), ParseError> {
+pub fn parse_input(input: &str) -> Result<(Procedure, CrateMap<Vec<Crate>>), ParseError> {
     let input_parts = input
         .split_once("\n\n")
         .expect(r"input should be separated by 2 newlines `\n\n`");
 
-    let map: CrateMap = input_parts.0.parse()?;
+    let map: CrateMap<Vec<Crate>> = input_parts.0.parse()?;
     let procedure: Procedure = input_parts.1.parse()?;
 
     Ok((procedure, map))
@@ -74,7 +125,7 @@ pub fn parse_input(input: &str) -> Result<(Procedure, CrateMap), ParseError> {
 
 #[cfg(test)]
 mod tests {
-    use super::{collect_message, rearrange_crates, Result};
+    use super::{collect_message, rearrange_crates, rearrange_crates_part2, Result};
     const INPUT: &str = include_str!("../input");
 
     const BASIC_EXAMPLE: &str = "    [D]    
@@ -108,8 +159,8 @@ move 1 from 1 to 2";
     }
 
     #[test]
-    fn part2_basic_example_should_result_in_cmz() -> Result<()> {
-        let map = rearrange_crates(BASIC_EXAMPLE)?;
+    fn part2_basic_example_should_result_in_mcd() -> Result<()> {
+        let map = rearrange_crates_part2(BASIC_EXAMPLE)?;
         let message = collect_message(map);
 
         assert_eq!(message, "MCD");
@@ -119,10 +170,10 @@ move 1 from 1 to 2";
 
     #[test]
     fn part2_input_should_result_in_value() -> Result<()> {
-        let map = rearrange_crates(INPUT)?;
+        let map = rearrange_crates_part2(INPUT)?;
         let message = collect_message(map);
 
-        assert_eq!(message, "ZBDRNPMVH");
+        assert_eq!(message, "WDLPFNNNB");
 
         Ok(())
     }
