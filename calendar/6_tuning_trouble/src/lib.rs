5b9041c0fc1e2db0a729a1e505ca7dfe41834954
Zach <zmriel@gmail.com>
2022-12-09 23:15:33 -0500

    day 6, part 2

--- calendar/6_tuning_trouble/src/lib.rs
+++ calendar/6_tuning_trouble/src/lib.rs
@@ -1,7 +1,5 @@
 use std::{collections::HashSet, ops::Range};
 
-const WINDOW_WIDTH: u8 = 4;
-
 /// Dynamic sliding window
 pub struct Window {
     /// index of the first char in the window
@@ -44,14 +42,14 @@ impl Window {
 }
 
 /// Get the index of the start-of-packet marker
-pub fn find_sop_marker(input: &str) -> usize {
-    let mut window = Window::new(WINDOW_WIDTH);
+pub fn find_sop_marker(input: &str, window_width: u8) -> usize {
+    let mut window = Window::new(window_width);
 
     while window.rear() < input.len() {
         if let Some(value) = input.get(window.as_range()) {
             let set: HashSet<char> = HashSet::from_iter(value.chars());
             // if set and length are the same, we have no duplicates!
-            if set.len() == window.len() {
+            if set.len() == (window_width as usize) {
                 return window.rear();
             }
         }
@@ -66,19 +64,35 @@ pub fn find_sop_marker(input: &str) -> usize {
 mod tests {
     use crate::find_sop_marker;
 
-    const INPUT_RESULTS: [(&str, usize); 6] = [
-        ("mjqjpqmgbljsphdztnvjfqwrcgsmlb", 7),
-        ("bvwbjplbgvbhsrlpgdmjqwftvncz", 5),
-        ("nppdvjthqldpwncqszvftbrmjlhg", 6),
-        ("nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg", 10),
-        ("zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw", 11),
-        (include_str!("../input"), 1804),
+    const WINDOW_WIDTH: u8 = 4;
+    const WINDOW_WIDTH_PART2: u8 = 14;
+
+    const INPUTS: [&str; 6] = [
+        "mjqjpqmgbljsphdztnvjfqwrcgsmlb",
+        "bvwbjplbgvbhsrlpgdmjqwftvncz",
+        "nppdvjthqldpwncqszvftbrmjlhg",
+        "nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg",
+        "zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw",
+        include_str!("../input"),
     ];
+
+    const RESULTS: [usize; 6] = [7, 5, 6, 10, 11, 1804];
+
+    const RESULTS_PART2: [usize; 6] = [19, 23, 23, 29, 26, 2508];
+
     #[test]
     fn find_sop_marker_should_take_input_and_find_result() {
-        for (input, expected_result) in INPUT_RESULTS {
-            let result = find_sop_marker(input);
-            assert_eq!(result, expected_result)
+        for (input, expected_result) in INPUTS.iter().zip(RESULTS.iter()) {
+            let result = find_sop_marker(input, WINDOW_WIDTH);
+            assert_eq!(result, *expected_result)
+        }
+    }
+
+    #[test]
+    fn part2_find_sop_marker_should_take_input_and_find_result() {
+        for (input, expected_result) in INPUTS.iter().zip(RESULTS_PART2.iter()) {
+            let result = find_sop_marker(input, WINDOW_WIDTH_PART2);
+            assert_eq!(result, *expected_result)
         }
     }
 }
