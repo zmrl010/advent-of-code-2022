9a3df4b3e91e81ccd6f513e7c43b16bce2286940
Zach <zmriel@gmail.com>
2022-12-13 07:44:22 -0500

    day 8 (in progress)

--- calendar/8_treetop_tree_house/src/grid.rs
+++ calendar/8_treetop_tree_house/src/grid.rs
@@ -5,8 +5,11 @@
 //! Credit to [this guide](https://blog.adamchalmers.com/grids-1/)
 //! for most of the implementation
 
+use rayon::prelude::*;
+use std::vec::IntoIter;
+
 /// Index for a 2D grid
-#[derive(Clone, Copy)]
+#[derive(Clone, Copy, Debug)]
 pub struct Point {
     pub x: usize,
     pub y: usize,
@@ -34,6 +37,7 @@ pub trait GridLike<T> {
         T: Send;
 }
 
+#[derive(Debug)]
 pub struct Grid<T> {
     items: Vec<T>,
     width: usize,
@@ -50,6 +54,41 @@ where
             width,
             height,
         }
+    }
+}
+
+impl<T> From<Vec<T>> for Grid<T> {
+    fn from(v: Vec<T>) -> Self {
+        let width = (v.len() as f64).sqrt().floor() as usize;
+
+        Self {
+            items: v,
+            width,
+            height: width,
+        }
+    }
+}
+
+impl<T> FromIterator<T> for Grid<T> {
+    fn from_iter<I: IntoIterator<Item = T>>(iter: I) -> Self {
+        let items: Vec<T> = iter.into_iter().collect();
+
+        let width = (items.len() as f64).sqrt().floor() as usize;
+
+        Self {
+            items,
+            width,
+            height: width,
+        }
+    }
+}
+
+impl<T> IntoIterator for Grid<T> {
+    type Item = T;
+    type IntoIter = IntoIter<T>;
+
+    fn into_iter(self) -> Self::IntoIter {
+        self.items.into_iter()
     }
 }
 
@@ -71,8 +110,6 @@ impl<T> GridLike<T> for Grid<T> {
         F: Send + Sync + Fn(Point) -> T,
         T: Send,
     {
-        use rayon::prelude::*;
-
         let width = self.width;
 
         self.items.par_iter_mut().enumerate().for_each(|(i, item)| {
