cbb0cd7ea1d442f0ec83554435b97fda739bfcf1
Zach <zmriel@gmail.com>
2023-01-30 15:19:12 -0500

    day 15, part one

--- calendar/15_beacon_exclusion_zone/src/lib.rs
+++ calendar/15_beacon_exclusion_zone/src/lib.rs
@@ -1,16 +1,14 @@
-use std::{
-    collections::{HashMap, HashSet},
-    default,
-};
+use std::{num::ParseIntError, ops::Range};
 
+use itertools::Itertools;
 use regex::Regex;
 
 #[macro_use]
 extern crate lazy_static;
 
-type Integer = isize;
+type Integer = i32;
 
-#[derive(Debug, Hash, PartialEq, Eq, PartialOrd, Ord, Clone, Copy)]
+#[derive(Debug, Default, PartialEq, Eq, Hash, Clone)]
 struct Point {
     x: Integer,
     y: Integer,
@@ -22,6 +20,15 @@ impl From<(Integer, Integer)> for Point {
     }
 }
 
+impl Point {
+    /// Calculate the [Manhattan distance] between this point and another
+    ///
+    /// [Manhattan distance]: https://en.wikipedia.org/wiki/Taxicab_geometry
+    fn manhattan_distance(&self, other: &Point) -> u32 {
+        self.x.abs_diff(other.x) + self.y.abs_diff(other.y)
+    }
+}
+
 #[derive(Debug, Hash)]
 struct Reading {
     sensor: Point,
@@ -29,73 +36,83 @@ struct Reading {
 }
 
 impl Reading {
-    fn new(position: Point, beacon: Point) -> Self {
-        Self {
-            sensor: position,
-            beacon,
-        }
+    fn new(sensor: Point, beacon: Point) -> Self {
+        Self { sensor, beacon }
+    }
+
+    /// Calculate distance between sensor and beacon
+    fn distance(&self) -> u32 {
+        self.sensor.manhattan_distance(&self.beacon)
+    }
+
+    fn covered_x_range(&self, row_index: Integer) -> Option<Range<Integer>> {
+        let Point { x, y } = self.sensor;
+        let x_offset = self.distance() as Integer - y.abs_diff(row_index) as Integer;
+
+        Some(x - x_offset..x + x_offset + 1).filter(|r| !r.is_empty())
     }
 }
 
-#[derive(Debug, Default)]
-enum Cell {
-    #[default]
-    Unknown,
-    Empty,
-    Sensor,
-    Beacon,
-}
-
-/// Calculate the manhattan distance between two points
-fn calculate_distance(a: Point, b: Point) -> usize {
-    a.x.abs_diff(b.x) + a.y.abs_diff(b.y)
-}
-
-/// Parse plot points into a Sensor from one line of input
-fn read_sensor(line: &str) -> anyhow::Result<Reading> {
+/// Extract integers from input into a [`Vec`]
+fn collect_numbers(input: &str) -> Result<Vec<Integer>, ParseIntError> {
     lazy_static! {
         static ref RE: Regex = Regex::new(r"-?\d+").unwrap();
     }
 
-    let coordinates: anyhow::Result<Vec<Integer>> = RE
-        .find_iter(line)
-        .map(|raw| Ok(raw.as_str().parse()?))
-        .collect();
+    RE.find_iter(input)
+        .map(|raw| raw.as_str().parse())
+        .collect()
+}
 
-    if let [sensor_x, sensor_y, beacon_x, beacon_y] = coordinates?[..] {
-        return Ok(Reading::new(
-            (sensor_x, sensor_y).into(),
-            (beacon_x, beacon_y).into(),
+/// Parse sensor reading from one line of input
+fn parse_line(line: &str) -> anyhow::Result<Reading> {
+    let values = collect_numbers(line)?;
+
+    if values.len() != 4 {
+        return Err(anyhow::anyhow!(
+            "expected input to contain 4 numbers\n\nReceived: `{line}`"
         ));
     }
 
-    Err(anyhow::anyhow!(
-        "expected 2 sets of coordinates in `{line}`"
+    Ok(Reading::new(
+        (values[0], values[1]).into(),
+        (values[2], values[3]).into(),
     ))
 }
 
-fn parse_input(input: &str) -> impl Iterator<Item = anyhow::Result<Reading>> + '_ {
-    input.lines().map(|line| read_sensor(line.trim()))
+fn parse_input(input: &str) -> anyhow::Result<Vec<Reading>> {
+    input.lines().map(|line| parse_line(line.trim())).collect()
 }
 
-pub fn part1(input: &str) -> u32 {
-    let mut cell_map = HashMap::<Point, Cell>::new();
-    let readings = parse_input(input);
+pub fn part1<const EXAMPLE_ROW: i32>(input: &str) -> anyhow::Result<usize> {
+    let readings = parse_input(input)?;
 
-    for reading in readings {
-        let reading = reading.unwrap();
+    let covered_xs: usize = readings
+        .iter()
+        .flat_map(|reading| reading.covered_x_range(EXAMPLE_ROW))
+        .sorted_unstable_by_key(|r| r.start)
+        .coalesce(|a, b| {
+            if a.end >= b.start {
+                Ok(a.start..b.end.max(a.end))
+            } else {
+                Err((a, b))
+            }
+        })
+        .map(|x| x.len())
+        .sum();
 
-        cell_map.insert(reading.sensor, Cell::Sensor);
-        cell_map.entry(reading.beacon).or_insert(Cell::Beacon);
+    let blocked_xs = readings
+        .into_iter()
+        .flat_map(|reading| [reading.sensor, reading.beacon])
+        .filter(|point| point.y == EXAMPLE_ROW)
+        .unique()
+        .count();
 
-        let distance = calculate_distance(reading.sensor, reading.beacon);
-    }
-
-    0
+    Ok(covered_xs - blocked_xs)
 }
 
-pub fn part2(input: &str) -> u32 {
-    0
+pub fn part2(_input: &str) -> anyhow::Result<usize> {
+    Ok(0)
 }
 
 #[cfg(test)]
@@ -106,30 +123,38 @@ mod tests {
     const INPUT: &str = include_str!("../input");
 
     #[test]
-    fn part1_basic_input_result_eq_expected() {
-        let result = part1(BASIC_INPUT);
+    fn part1_basic_input_result_eq_expected() -> anyhow::Result<()> {
+        let result = part1::<10>(BASIC_INPUT)?;
 
         assert_eq!(result, 26);
+
+        Ok(())
     }
 
     #[test]
-    fn part1_input_result_eq_expected() {
-        let result = part1(INPUT);
+    fn part1_input_result_eq_expected() -> anyhow::Result<()> {
+        let result = part1::<2_000_000>(INPUT)?;
+
+        assert_eq!(result, 5_838_453);
+
+        Ok(())
+    }
+
+    #[test]
+    fn part2_basic_input_result_eq_expected() -> anyhow::Result<()> {
+        let result = part2(BASIC_INPUT)?;
 
         assert_eq!(result, 0);
+
+        Ok(())
     }
 
     #[test]
-    fn part2_basic_input_result_eq_expected() {
-        let result = part2(BASIC_INPUT);
-
-        assert_eq!(result, 0)
-    }
-
-    #[test]
-    fn part2_input_result_eq_expected() {
-        let result = part2(INPUT);
+    fn part2_input_result_eq_expected() -> anyhow::Result<()> {
+        let result = part2(INPUT)?;
 
         assert_eq!(result, 0);
+
+        Ok(())
     }
 }
